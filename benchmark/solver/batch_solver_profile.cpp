e1d549f1cbce7ad618c388c966bca2c2d1aa4a83
Aditya Kashi <aditya.kashi@kit.edu>
2021-07-15 16:42:36 +0200

    second fix for benchmark includes

--- benchmark/solver/batch_solver_profile.cpp
+++ benchmark/solver/batch_solver_profile.cpp
@@ -30,9 +30,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ******************************<GINKGO LICENSE>*******************************/
 
-// clang-format off
-#include "batch_solver.hpp"
-// clang-format on
+#include "./batch_solver.hpp"
 
 
 int main(int argc, char *argv[])
