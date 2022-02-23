c992add26cfba83bde2d89c7a7f31add52d3806d
Aditya Kashi <aditya.kashi@kit.edu>
2022-02-23 08:33:51 +0100

    commented out cerr statements in batch bcgs

--- hip/solver/batch_bicgstab_kernels.hip.cpp
+++ hip/solver/batch_bicgstab_kernels.hip.cpp
@@ -144,17 +144,17 @@ static void apply_impl(
         exec, sconf.gmem_stride_bytes * nbatch / sizeof(ValueType));
     assert(sconf.gmem_stride_bytes % sizeof(ValueType) == 0);
 
-    std::cerr << " Bicgstab: vectors in shared memory = " << sconf.n_shared
-              << "\n";
-    if (sconf.prec_shared) {
-        std::cerr << " Bicgstab: precondiioner is in shared memory.\n";
-    }
-    std::cerr << " Bicgstab: vectors in global memory = " << sconf.n_global
-              << "\n";
-    std::cerr << " Hip: number of threads per warp = " << config::warp_size
-              << "\n";
-    std::cerr << " Bicgstab: number of threads per block = " << block_size
-              << "\n";
+    // std::cerr << " Bicgstab: vectors in shared memory = " << sconf.n_shared
+    //           << "\n";
+    // if (sconf.prec_shared) {
+    //     std::cerr << " Bicgstab: precondiioner is in shared memory.\n";
+    // }
+    // std::cerr << " Bicgstab: vectors in global memory = " << sconf.n_global
+    //           << "\n";
+    // std::cerr << " Hip: number of threads per warp = " << config::warp_size
+    //           << "\n";
+    // std::cerr << " Bicgstab: number of threads per block = " << block_size
+    //           << "\n";
     if (opts.tol_type == gko::stop::batch::ToleranceType::absolute) {
         BATCH_BICGSTAB_KERNEL_LAUNCH(SimpleAbsResidual, PrecType);
     } else {
