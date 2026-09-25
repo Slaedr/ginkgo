4cae599033a1992c34919e1860750795ba0c9d9c
Aditya Kashi <kashia@ornl.gov>
2026-09-24 21:09:27 -0400

    fgs (csr) solver kernels use subwarps based on max row length

--- common/cuda_hip/solver/gauss_seidel_kernels.cpp
+++ common/cuda_hip/solver/gauss_seidel_kernels.cpp
@@ -15,6 +15,7 @@
 #include "common/unified/matrix/amp_algorithms.hpp"
 #include "core/base/mixed_precision_types.hpp"
 #include "core/base/utils.hpp"
+#include "core/synthesizer/implementation_selection.hpp"
 
 
 namespace gko {
@@ -31,6 +32,10 @@ namespace gkerd = gko::kernels::GKO_DEVICE_NAMESPACE;
 using gkerd::reduce;
 
 constexpr int default_block_size = 1024;
+
+constexpr uint32 classical_oversubscription = 8;
+using classical_kernels =
+    syn::value_list<int, config::warp_size, 32, 16, 8, 4, 2, 1>;
 
 template <typename InputValueType, typename MatrixValueType,
           typename OutputValueType, typename IndexType>
@@ -257,10 +262,9 @@ GKO_INSTANTIATE_FOR_EACH_MIXED_VALUE_AND_INDEX_TYPE_BASE(
     GKO_DECLARE_MULTICOLOR_FWD_GS_AMP_KERNEL);
 
 
-constexpr int warp_size = config::warp_size;
-constexpr int warps_per_block = default_block_size / warp_size;
+constexpr int warps_per_block = default_block_size / config::warp_size;
 
-template <typename InputValueType, typename MatrixValueType,
+template <int subwarp_size, typename InputValueType, typename MatrixValueType,
           typename OutputValueType, typename IndexType>
 __global__ __launch_bounds__(default_block_size) void mc_fgs_csr(
     const IndexType* const __restrict__ row_ptrs,
@@ -273,59 +277,62 @@ __global__ __launch_bounds__(default_block_size) void mc_fgs_csr(
 {
     using highest_type = gko::highest_precision<InputValueType, MatrixValueType,
                                                 OutputValueType>;
-    const auto warp =
-        group::tiled_partition<warp_size>(group::this_thread_block());
-    const auto lane = warp.thread_rank();
-    const auto warp_id = thread::get_subwarp_id_flat<warp_size, IndexType>();
-    const auto row = begin_row + warp_id;
-    if (row >= end_row) {
-        return;
-    }
+    const auto subwarp =
+        group::tiled_partition<subwarp_size>(group::this_thread_block());
+    const auto lane = subwarp.thread_rank();
+    const auto subwarp_id =
+        thread::get_subwarp_id_flat<subwarp_size, IndexType>();
+    const auto num_subwarps = thread::get_subwarp_num_flat<subwarp_size>();
     const auto irhs = blockIdx.y;
-    if (first_iter && warp_id == 0 && lane == 0) {
+    if (first_iter && subwarp_id == 0 && lane == 0) {
         stopstatus[irhs].reset();
     }
 
-    const auto row_start = row_ptrs[row];
-    const auto row_end_nnz = row_ptrs[row + 1];
-    auto partial_sum = zero<highest_type>();
-    auto diag = zero<MatrixValueType>();
+    for (auto row = begin_row + subwarp_id; row < end_row;
+         row += num_subwarps) {
+        const auto row_start = row_ptrs[row];
+        const auto row_end_nnz = row_ptrs[row + 1];
+        auto partial_sum = zero<highest_type>();
+        auto diag = zero<MatrixValueType>();
 
-    for (auto k = row_start + lane; k < row_end_nnz; k += warp_size) {
-        const auto col = col_idxs[k];
-        const auto val = values[k];
-        if (col == row) {
-            diag = val;
-        } else {
-            partial_sum -= static_cast<highest_type>(val) *
-                           static_cast<highest_type>(x[col * x_stride + irhs]);
+        for (auto k = row_start + lane; k < row_end_nnz; k += subwarp_size) {
+            const auto col = col_idxs[k];
+            const auto val = values[k];
+            if (col == row) {
+                diag = val;
+            } else {
+                partial_sum -=
+                    static_cast<highest_type>(val) *
+                    static_cast<highest_type>(x[col * x_stride + irhs]);
+            }
         }
-    }
 
-    auto sum = reduce(warp, partial_sum,
-                      [](highest_type a, highest_type b) { return a + b; });
-    auto warp_diag = reduce(
-        warp, diag, [](MatrixValueType a, MatrixValueType b) { return a + b; });
+        auto sum = reduce(subwarp, partial_sum,
+                          [](highest_type a, highest_type b) { return a + b; });
+        auto subwarp_diag =
+            reduce(subwarp, diag,
+                   [](MatrixValueType a, MatrixValueType b) { return a + b; });
 
-    if (lane == 0) {
-        sum += static_cast<highest_type>(b[row * b_stride + irhs]);
-        if (warp_diag != zero<MatrixValueType>()) {
-            x[row * x_stride + irhs] = static_cast<OutputValueType>(
-                sum / static_cast<highest_type>(warp_diag));
+        if (lane == 0) {
+            sum += static_cast<highest_type>(b[row * b_stride + irhs]);
+            if (subwarp_diag != zero<MatrixValueType>()) {
+                x[row * x_stride + irhs] = static_cast<OutputValueType>(
+                    sum / static_cast<highest_type>(subwarp_diag));
+            }
         }
     }
 }
 
 
-template <typename InputValueType, typename MatrixValueType,
+template <int subwarp_size, typename InputValueType, typename MatrixValueType,
           typename OutputValueType, typename IndexType>
-void multicolor_fgs_csr(std::shared_ptr<const DefaultExecutor> exec,
-                        const std::vector<IndexType>& color_ptrs,
-                        const matrix::Csr<MatrixValueType, IndexType>* const a,
-                        const matrix::Dense<InputValueType>* const b,
-                        matrix::Dense<OutputValueType>* const x,
-                        const bool first_iter,
-                        array<stopping_status>* const stop_status)
+void mc_fgs_csr(syn::value_list<int, subwarp_size>,
+                std::shared_ptr<const DefaultExecutor> exec,
+                const std::vector<IndexType>& color_ptrs,
+                const matrix::Csr<MatrixValueType, IndexType>* const a,
+                const matrix::Dense<InputValueType>* const b,
+                matrix::Dense<OutputValueType>* const x, const bool first_iter,
+                array<stopping_status>* const stop_status)
 {
     if (color_ptrs.size() < 2) {
         return;
@@ -344,14 +351,22 @@ void multicolor_fgs_csr(std::shared_ptr<const DefaultExecutor> exec,
     const auto x_stride = x->get_stride();
     const auto b_stride = b->get_stride();
 
+    const auto nwarps = static_cast<uint32>(exec->get_num_warps_per_sm() *
+                                            exec->get_num_multiprocessor()) *
+                        classical_oversubscription;
+
     for (int color = 0; color < num_colors; ++color) {
         const auto row_begin = color_ptrs[color];
         const auto row_end = color_ptrs[color + 1];
         const auto nrows = row_end - row_begin;
+        const auto num_blocks =
+            std::min(static_cast<uint32>(
+                         ceildiv(nrows, default_block_size / subwarp_size)),
+                     nwarps / warps_per_block);
         // One warp per row: warps_per_block rows per block
-        const dim3 nblocks{static_cast<uint32>(ceildiv(nrows, warps_per_block)),
-                           static_cast<uint32>(num_rhs), 1u};
-        mc_fgs_csr<d_i_val_type, d_m_val_type, d_o_val_type, IndexType>
+        const dim3 nblocks{num_blocks, static_cast<uint32>(num_rhs), 1u};
+        mc_fgs_csr<subwarp_size, d_i_val_type, d_m_val_type, d_o_val_type,
+                   IndexType>
             <<<nblocks, default_block_size, 0, exec->get_stream()>>>(
                 row_ptrs_dev, col_idxs_dev, values_dev, row_begin, row_end,
                 b_stride, b_vals, x_stride, x_vals, stop_status->get_data(),
@@ -359,12 +374,65 @@ void multicolor_fgs_csr(std::shared_ptr<const DefaultExecutor> exec,
     }
 }
 
+GKO_ENABLE_IMPLEMENTATION_SELECTION(select_mc_fgs_csr, mc_fgs_csr);
+
+
+template <typename InputValueType, typename MatrixValueType,
+          typename OutputValueType, typename IndexType>
+void multicolor_fgs_csr(std::shared_ptr<const DefaultExecutor> exec,
+                        const std::vector<IndexType>& color_ptrs,
+                        const matrix::Csr<MatrixValueType, IndexType>* const a,
+                        const matrix::Dense<InputValueType>* const b,
+                        matrix::Dense<OutputValueType>* const x,
+                        const bool first_iter,
+                        array<stopping_status>* const stop_status)
+{
+    IndexType max_length_per_row = 0;
+    using Tcsr = matrix::Csr<MatrixValueType, IndexType>;
+    if (auto strategy =
+            std::dynamic_pointer_cast<const typename Tcsr::classical>(
+                a->get_strategy())) {
+        max_length_per_row = strategy->get_max_length_per_row();
+    } else if (auto strategy =
+                   std::dynamic_pointer_cast<const typename Tcsr::automatical>(
+                       a->get_strategy())) {
+        max_length_per_row = strategy->get_max_length_per_row();
+    } else {
+        // as a fall-back: use average row length, at least 1
+        max_length_per_row = a->get_num_stored_elements() /
+                             std::max<size_type>(a->get_size()[0], 1);
+    }
+    max_length_per_row = std::max<size_type>(max_length_per_row, 1);
+    select_mc_fgs_csr(
+        classical_kernels(),
+        [&max_length_per_row](int compiled_len) {
+            return max_length_per_row >= compiled_len;
+        },
+        syn::value_list<int>(), syn::type_list<>(), exec, color_ptrs, a, b, x,
+        first_iter, stop_status);
+}
+
 GKO_INSTANTIATE_FOR_EACH_MIXED_VALUE_AND_INDEX_TYPE_BASE(
     GKO_DECLARE_MULTICOLOR_FWD_GS_CSR_KERNEL);
 
 
-template <typename IValueType, typename MValueType, typename OValueType,
-          typename IndexType>
+template <typename ValueType, typename IndexType>
+static inline IndexType eff_max_row_len(
+    const matrix::AMP<ValueType, IndexType>* const a)
+{
+    constexpr int q = matrix::AMP<ValueType, IndexType>::num_precisions;
+    auto maxnr = zero<IndexType>();
+    for (int k = 0; k < q; k++) {
+        auto binmax = a->get_max_nnz_per_row_for_bin(k);
+        maxnr += binmax;
+    }
+    maxnr /= q;
+    return maxnr;
+}
+
+
+template <int subwarp_size, typename IValueType, typename MValueType,
+          typename OValueType, typename IndexType>
 __global__ __launch_bounds__(default_block_size) void mc_fgs_amp_csr(
     const amp::precision_array<const IndexType*, MValueType> bin_row_ptrs,
     const amp::precision_array<const IndexType*, MValueType> bin_col_idxs,
@@ -377,64 +445,69 @@ __global__ __launch_bounds__(default_block_size) void mc_fgs_amp_csr(
     constexpr int q = amp::narrow_types<MValueType>::num_types;
     using highest_type =
         gko::highest_precision<IValueType, MValueType, OValueType>;
-    const auto warp =
-        group::tiled_partition<warp_size>(group::this_thread_block());
-    const auto lane = warp.thread_rank();
-    const auto warp_id = thread::get_subwarp_id_flat<warp_size, IndexType>();
-    const auto row = begin_row + warp_id;
-    if (row >= end_row) {
-        return;
-    }
+    const auto subwarp =
+        group::tiled_partition<subwarp_size>(group::this_thread_block());
+    const auto lane = subwarp.thread_rank();
+    const auto subwarp_id =
+        thread::get_subwarp_id_flat<subwarp_size, IndexType>();
+    const auto num_subwarps =
+        thread::get_subwarp_num_flat<subwarp_size, IndexType>();
+
     const auto irhs = blockIdx.y;
-    if (first_iter && warp_id == 0 && lane == 0) {
+    if (first_iter && subwarp_id == 0 && lane == 0) {
         stopstatus[irhs].reset();
     }
 
-    auto partial_sum = zero<highest_type>();
-    auto diag = zero<MValueType>();
+    for (auto row = begin_row + subwarp_id; row < end_row;
+         row += num_subwarps) {
+        auto partial_sum = zero<highest_type>();
+        auto diag = zero<MValueType>();
 
-    gko::constexpr_for<0, q, 1>([&](auto k) {
-        const auto row_start = bin_row_ptrs[k][row];
-        const auto row_end_nnz = bin_row_ptrs[k][row + 1];
-        auto avals = std::get<k>(bin_values);
-        auto acols = bin_col_idxs[k];
-        for (auto j = row_start + lane; j < row_end_nnz; j += warp_size) {
-            const auto col = acols[j];
-            const auto val = avals[j];
-            if (col == row) {
-                diag = static_cast<MValueType>(val);
-            } else {
-                partial_sum -=
-                    static_cast<highest_type>(val) *
-                    static_cast<highest_type>(x[col * x_stride + irhs]);
+        gko::constexpr_for<0, q, 1>([&](auto k) {
+            const auto row_start = bin_row_ptrs[k][row];
+            const auto row_end_nnz = bin_row_ptrs[k][row + 1];
+            auto avals = std::get<k>(bin_values);
+            auto acols = bin_col_idxs[k];
+            for (auto j = row_start + lane; j < row_end_nnz;
+                 j += subwarp_size) {
+                const auto col = acols[j];
+                const auto val = avals[j];
+                if (col == row) {
+                    diag = static_cast<MValueType>(val);
+                } else {
+                    partial_sum -=
+                        static_cast<highest_type>(val) *
+                        static_cast<highest_type>(x[col * x_stride + irhs]);
+                }
             }
-        }
-    });
+        });
 
-    auto sum = reduce(warp, partial_sum,
-                      [](highest_type a, highest_type b) { return a + b; });
-    auto warp_diag =
-        reduce(warp, diag, [](MValueType a, MValueType b) { return a + b; });
+        auto sum = reduce(subwarp, partial_sum,
+                          [](highest_type a, highest_type b) { return a + b; });
+        auto subwarp_diag = reduce(
+            subwarp, diag, [](MValueType a, MValueType b) { return a + b; });
 
-    if (lane == 0) {
-        sum += static_cast<highest_type>(b[row * b_stride + irhs]);
-        if (warp_diag != zero<MValueType>()) {
-            x[row * x_stride + irhs] = static_cast<OValueType>(
-                sum / static_cast<highest_type>(warp_diag));
+        if (lane == 0) {
+            sum += static_cast<highest_type>(b[row * b_stride + irhs]);
+            if (subwarp_diag != zero<MValueType>()) {
+                x[row * x_stride + irhs] = static_cast<OValueType>(
+                    sum / static_cast<highest_type>(subwarp_diag));
+            }
         }
     }
 }
 
 
-template <typename InputValueType, typename MatrixValueType,
+template <int subwarp_size, typename InputValueType, typename MatrixValueType,
           typename OutputValueType, typename IndexType>
-void multicolor_fgs_amp_csr(
-    std::shared_ptr<const DefaultExecutor> exec,
-    const std::vector<IndexType>& color_ptrs,
-    const matrix::AMP<MatrixValueType, IndexType>* const a,
-    const matrix::Dense<InputValueType>* const b,
-    matrix::Dense<OutputValueType>* const x, const bool first_iter,
-    array<stopping_status>* const stop_status)
+void mc_fgs_amp_csr(syn::value_list<int, subwarp_size>,
+                    std::shared_ptr<const DefaultExecutor> exec,
+                    const std::vector<IndexType>& color_ptrs,
+                    const matrix::AMP<MatrixValueType, IndexType>* const a,
+                    const matrix::Dense<InputValueType>* const b,
+                    matrix::Dense<OutputValueType>* const x,
+                    const bool first_iter,
+                    array<stopping_status>* const stop_status)
 {
     if (color_ptrs.size() < 2) {
         return;
@@ -468,17 +541,48 @@ void multicolor_fgs_amp_csr(
         std::get<k>(avalues) = as_device_type(cmatk->get_const_values());
     });
 
+    const auto nwarps = static_cast<uint32>(exec->get_num_warps_per_sm() *
+                                            exec->get_num_multiprocessor()) *
+                        classical_oversubscription;
+
     for (int color = 0; color < num_colors; ++color) {
         const auto row_begin = color_ptrs[color];
         const auto row_end = color_ptrs[color + 1];
         const auto nrows = row_end - row_begin;
-        const dim3 nblocks{static_cast<uint32>(ceildiv(nrows, warps_per_block)),
-                           static_cast<uint32>(num_rhs), 1u};
-        mc_fgs_amp_csr<d_i_val_type, d_m_val_type, d_o_val_type, IndexType>
+        const auto num_blocks =
+            std::min(static_cast<uint32>(
+                         ceildiv(nrows, default_block_size / subwarp_size)),
+                     nwarps / warps_per_block);
+        const dim3 nblocks{num_blocks, static_cast<uint32>(num_rhs), 1u};
+        mc_fgs_amp_csr<subwarp_size, d_i_val_type, d_m_val_type, d_o_val_type,
+                       IndexType>
             <<<nblocks, default_block_size, 0, exec->get_stream()>>>(
                 arow_ptrs, acol_idxs, avalues, row_begin, row_end, b_stride,
                 b_vals, x_stride, x_vals, stop_status->get_data(), first_iter);
     }
+}
+
+GKO_ENABLE_IMPLEMENTATION_SELECTION(select_mc_fgs_amp_csr, mc_fgs_amp_csr);
+
+
+template <typename InputValueType, typename MatrixValueType,
+          typename OutputValueType, typename IndexType>
+void multicolor_fgs_amp_csr(
+    std::shared_ptr<const DefaultExecutor> exec,
+    const std::vector<IndexType>& color_ptrs,
+    const matrix::AMP<MatrixValueType, IndexType>* const a,
+    const matrix::Dense<InputValueType>* const b,
+    matrix::Dense<OutputValueType>* const x, const bool first_iter,
+    array<stopping_status>* const stop_status)
+{
+    const int sel_row_len = std::max(eff_max_row_len(a), one<IndexType>());
+    select_mc_fgs_amp_csr(
+        classical_kernels(),
+        [&sel_row_len](int compiled_row_len) {
+            return sel_row_len >= compiled_row_len;
+        },
+        syn::value_list<int>(), syn::type_list<>(), exec, color_ptrs, a, b, x,
+        first_iter, stop_status);
 }
 
 GKO_INSTANTIATE_FOR_EACH_MIXED_VALUE_AND_INDEX_TYPE_BASE(
